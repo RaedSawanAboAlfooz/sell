d62e8488f8bb46508a12b6fb7d6b66e308ff062e
Raeed Swan <85413451+RaedSawanAboAlfooz@users.noreply.github.com>
2021-10-05 15:37:17 +0300

    fd

--- config/filesystems.php
+++ config/filesystems.php
@@ -37,8 +37,8 @@ return [
 
         'public' => [
             'driver' => 'local',
-            'root' => storage_path(env('STORAGE_PATH')),
-            'url' => env('APP_URL'),
+            'root' => storage_path('app/public'),
+            'url' => env('APP_URL').env('STORAGE_PATH'),
             'visibility' => 'public',
         ],
 
