3e30b65a591b1bb278705566ad6411b8b00559fb
RaedSawanAboAlfooz <raedswangithub1@gmail.com>
2021-10-05 13:40:33 +0300

    chang

--- resources/views/category/edit.blade.php
+++ resources/views/category/edit.blade.php
@@ -22,7 +22,7 @@
             <label  for="exampleInputEmail1" class="form-label">Image category</label><br><br>
             {{-- <input type="file" class="form-control"  name="name" placeholder="{{$category->name}}"  id="exampleInputEmail1" aria-describedby="emailHelp"> --}}
             <input type="file"  name="url_image" multiple><br><br>
-           <center> <img src="{{asset('assets/images/Categoryd/'.$category->name_image)}}" alt="{{asset('assets/images/Category/'.$category->name_image)}}" width="300px"></center>
+           <center> <img src="{{asset('assets/images/Category/'.$category->name_image)}}" alt="{{asset('assets/images/Category/'.$category->name_image)}}" width="300px"></center>
          </div>
         <button type="submit" class="btn btn-primary">update</button>
         @csrf
